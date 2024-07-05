dae45e0811c750520ab8490c0cb2f0387bc55a5d
TranHaGiaHuy <huythgce170636@fpt.edu.vn>
2024-07-05 14:43:38 +0700

    Fixing prop generate

--- Assets/Scripts/Map/PropRandomizer.cs
+++ Assets/Scripts/Map/PropRandomizer.cs
@@ -16,9 +16,10 @@ public class PropGenerizer : MonoBehaviour
     {
         foreach (GameObject sp in propSpawnPoints)
         { 
-            int rand = Random.Range(0, propSpawnPoints.Count);
+            int rand = Random.Range(0, propPrefabs.Count);
             GameObject prop = Instantiate(propPrefabs[rand], sp.transform.position,Quaternion.identity);
             prop.transform.parent = sp.transform;
         }
     }
-}
+} 
+
